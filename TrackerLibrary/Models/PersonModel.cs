506711a16b040c71ae26a54b284aeffa9cfef91a
Amrit <AmritMaan2001@gmail.com>
2023-02-14 01:30:41 -0500

    Add project files.

---
+++ TrackerLibrary/Models/PersonModel.cs
@@ -0,0 +1,37 @@
+﻿namespace TrackerLibrary.Models
+{
+    /// <summary>
+    /// Represents one person
+    /// </summary>
+    public class PersonModel
+    {
+        /// <summary>
+        /// The unique identifier for the person
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// The first name of the person
+        /// </summary>
+        public string FirstName { get; set; } = string.Empty;
+        /// <summary>
+        /// the Last Name of the person
+        /// </summary>
+        public string Lastname { get; set; } = string.Empty;
+        /// <summary>
+        /// The primary email address of the person
+        /// </summary>
+        public string EmailAddress { get; set; } = string.Empty; 
+        /// <summary>
+        /// The primary cell phone number of the person
+        /// </summary>
+        public string CellPhoneNumber { get; set; } = string.Empty; 
+
+        public string FullName
+        {
+            get
+            {
+                return $"{FirstName} {Lastname}";
+            }
+        }
+    }
+}
---
+++ TrackerLibrary/Models/TeamModel.cs
@@ -0,0 +1,12 @@
+﻿namespace TrackerLibrary.Models
+{
+    namespace TrackerLibrary.Models
+    {
+        public class TeamModel
+        {
+            public int Id { get; set; }
+            public string TeamName { get; set; } = string.Empty;
+            public List<PersonModel> TeamMembers { get; set; } = new List<PersonModel>();
+        }
+    }
+}
